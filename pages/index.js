60bbccea6d0a9f20094433941f990e0c015f8999
Christoph Leuker <cleuker87@gmail.com>
2023-06-06 11:47:15 +0200

    solution

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+// import BoxWithStyledComponents from "../components/BoxWithStyledComponents.js";
+import styled from "styled-components";
+import StyledBox from "../components/BoxWithStyledComponents.js";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  background-color: yellow;
+`;
 
 export default function HomePage() {
   return (
-    <div>
+    <Container>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-    </div>
+      <StyledBox />
+      <StyledBox isBlack />
+    </Container>
   );
 }
